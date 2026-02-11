b0afb2adf11d3bcd10460f2498617adfd82c34f3
Karunasindhu Adak <karunasindhuadak7@gmail.com>
2026-02-11 19:32:37 +0530

    add getUserProfile function to fetch user profile details with subscription info

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -366,6 +366,74 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "Coverimage updated succesfully"));
 });
 
+const getUserProfile = asyncHandler(async (req, res) => {
+  const {username} = req.params
+  if(!username?.trim()) {
+    throw new ApiError(401, "Username not exist")
+  }
+
+  const channal = await User.aggregate([
+    {
+      $match: {
+        username: username.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
+        },
+        subscribedToCount: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        email: 1,
+        avatar: 1,
+        coverImage: 1,
+        subscribersCount: 1,
+        subscribedToCount: 1,
+        isSubscribed: 1,
+      }
+    }
+  ]);
+
+  if(!channal || channal.length === 0) {
+    throw new ApiError(404, "User not found")
+  }
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, channal[0], "User profile fetched succesfully"))
+})
+
 export {
   registerUser,
   loginUser,
@@ -376,4 +444,5 @@ export {
   updateUserDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserProfile
 };
